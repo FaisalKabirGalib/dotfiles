9c000bacdbe3d2d136f62e5297990b7c463c4e6f
Faisal Kabir <faisalkabirgalib@gmail.com>
2024-09-01 11:20:03 +0600

    alacrity opacity ,codium and go added

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim",                                import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.coding.codeium" },
