337996ea9168ad8e24babe64eba2881748f812f0
Faisal Kabir <faisalkabirgalib@gmail.com>
2026-09-06 09:30:20 +0600

    fix herdr

--- omarchy/.config/hypr/autostart.lua
+++ omarchy/.config/hypr/autostart.lua
@@ -1,4 +1,4 @@
 -- Extra autostart processes.
 
-o.exec_on_start("[workspace 11 silent] ghostty -e tmux new -A -s Home")
+o.exec_on_start("[workspace 11 silent] ghostty -e herdr")
 o.exec_on_start("[workspace 12 silent] zen-browser")
